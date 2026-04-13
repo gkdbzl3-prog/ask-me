a9d9344cccc20587927f0c04e00d85de696412e4
nal <gkdbzl3@gmail.com>
2026-04-13 09:33:15 +0900

    오타 수정

--- routes/archive.js
+++ routes/archive.js
@@ -73,66 +73,33 @@ function buildHashtagGroups(rawPosts) {
 }
 
 
-router.get("/hashtags", (req,res) => {
+router.get("/hashtags", async (req,res) => {
  try {
   const ownerId = req.query.ownerId || "";
   const username = req.query.username || "";
 
-//   const accessToken = "유저 access token";
-//   const userId = ownerId;
+  const accessToken = "유저 access token";
+  const userId = ownerId;
 
-//   const xRes = await fetch(
-// `https://api.x.com/2/users/${userId}/tweets?max_results=20&expansions=attachments.media_keys&tweet.fields=
-// attachments,text&media.fields=uri,type",
-//   {
-//     headers: {
-//       Authorization: `Bearer ${accessToken}`,
-//     },
-//   }
-//  );
-
-//  const xJson = await xRes.json();
-
-//  const raqPosts = mapXPostsToRawPosts(
-//   xJson.data,
-//   xJson.includes,
-//   username
-//  );
-
-  const mockXResponse = {
-    data: [
-      {
-      id: "1001",
-      text: "오늘의 다꾸 #날이_좋은_날 #루틴",
-      attachments: { media_keys: ["3_aaa","3_bbb"] },
-      },
-      {
-      id: "1002",
-      text: "공부 끝 #공부기록 #루틴",
-      attachments: { media_keys: ["3_ccc"] },
-      },
-      {id: "1003",
-      text: "정리중 #공부기록",
-      attachments: { media_keys: ["3_add", "3_eee"] },
-      },
-    ],
-    includes: {
-      media: [
-        {media_key: "3_aaa", type: "photo", url: "/images/sample1.jpg"},
-        {media_key: "3_bbb", type: "photo", url: "/images/sample2.jpg"},
-        {media_key: "3_ccc", type: "photo", url: "/images/sample3.jpg"},
-        {media_key: "3_ddd", type: "photo", url: "/images/sample4.jpg"},
-        {media_key: "3_eee", type: "photo", url: "/images/sample5.jpg"},
-      ],
+  const xRes = await fetch(
+`https://api.x.com/2/users/${userId}/tweets?max_results=20&expansions=attachments.media_keys&tweet.fields=attachments,text&media.fields=url,type`,
+  {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
     },
-  };
+  }
+ );
+ console.log("xRes status:", xRes.status);
 
+ const xJson = await xRes.json();
+ console.log("xJson:",JSON.stringify(xJson, null, 2));
 
  const rawPosts = mapXPostsToRawPosts(
-  mockXResponse.data,
-  mockXResponse.includes,
+  xJson.data,
+  xJson.includes,
   username
  );
+
 
  const groupedHashtags = buildHashtagGroups(rawPosts);
 
