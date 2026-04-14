85fd633386479dfed21ad1afe7b6d5aa19a5cec6
nal <gkdbzl3@gmail.com>
2026-04-14 20:07:36 +0900

    jsconfig 지우기

--- supabase.js
+++ supabase.js
@@ -1,6 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceKey = process.env.SPABASE_SERVICE_ROLE_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl) {
+ throw new Error("SUPABASE_URL is missing");
+}
+
+if (!supabaseServiceKey) {
+ throw new Error("SUPABASE_SERVICE_ROLE_KEY is missing");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
