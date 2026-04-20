81b948fba3ff5ba5aa9bfdfe09d7ab5534db1728
nal <gkdbzl3@gmail.com>
2026-04-20 22:47:36 +0900

    vite.config 수정

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      "/api": "http://localhost:8080",
+      "/auth": "http://localhost:8080",
+      "/archive": "http://localhost:8080"
+    },
+  },
 });
