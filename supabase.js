603d4685438d47ca5f9ebcb93a3eb6733a396f06
nal <gkdbzl3@gmail.com>
2026-04-20 21:37:13 +0900

    dotenv 붙이기

--- supabase.js
+++ supabase.js
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
